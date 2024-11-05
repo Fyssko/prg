cd51de721533b1f567138c321f9a0f88513cf32f
Fyssko <95652491+Fyssko@users.noreply.github.com>
2024-11-05 12:16:20 +0100

    Create SERVER.php

---
+++ SERVER.php
@@ -0,0 +1,3 @@
+echo $_SERVER["SERVER_NAME"]; // Vrátí název serveru (např. "example.com")
+echo $_SERVER["REQUEST_METHOD"]; // Vrátí metodu požadavku ("GET","POST",...)
+echo $_SERVER["REMOTE_ADDR"]; // Vrátí IP uživatele který požádal o stránku
